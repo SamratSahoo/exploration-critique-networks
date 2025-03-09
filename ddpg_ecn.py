60e1d95c49ce75709cab8c3c86e61b85196f0759
Samrat Sahoo <samratsahoo2013@gmail.com>
2025-03-09 14:54:33 -0400

    add state aggregation implementation

--- ddpg_ecn.py
+++ ddpg_ecn.py
@@ -12,7 +12,15 @@ import torch.nn.functional as F
 import torch.optim as optim
 import tyro
 from stable_baselines3.common.buffers import ReplayBuffer
-from torch.utils.tensorboard import SummaryWriter
+
+import stable_baselines3 as sb3
+
+if sb3.__version__ < "2.0":
+    raise ValueError(
+        """Ongoing migration: run the following command to install the new dependencies:
+poetry run pip install "stable_baselines3==2.0.0a1"
+"""
+    )
 
 
 @dataclass
@@ -51,15 +59,32 @@ class Args:
     """timestep to start learning"""
     policy_frequency: int = 2
     """the frequency of training policy (delayed)"""
+    print_log_frequency: int = 100
+    """the frequency of printed_logs"""
     noise_clip: float = 0.5
     """noise clip parameter of the Target Policy Smoothing Regularization"""
+
+
+args = tyro.cli(Args)
+run_name = f"runs/{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+os.makedirs(run_name, exist_ok=True)
+os.makedirs(f"{run_name}/models", exist_ok=True)
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.backends.cudnn.deterministic = args.torch_deterministic
+
+device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+torch.set_default_device(device)
 
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
         if capture_video and idx == 0:
             env = gym.make(env_id, render_mode="rgb_array")
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+            env = gym.wrappers.RecordVideo(
+                env, f"videos/{run_name.replace('runs/', '')}"
+            )
             env.recorded_frames = []
         else:
             env = gym.make(env_id)
@@ -70,13 +95,18 @@ def make_env(env_id, seed, idx, capture_video, run_name):
     return thunk
 
 
+envs = gym.vector.SyncVectorEnv(
+    [make_env(args.env_id, args.seed, 0, args.capture_video, run_name)]
+)
+
+
 # ALGO LOGIC: initialize agent here:
-class QNetwork(nn.Module):
+class Critic(nn.Module):
     def __init__(self, env):
         super().__init__()
+        self.obs_space_size = np.array(env.single_observation_space.shape).prod()
         self.fc1 = nn.Linear(
-            np.array(env.single_observation_space.shape).prod()
-            + np.prod(env.single_action_space.shape),
+            self.obs_space_size + np.prod(env.single_action_space.shape),
             256,
         )
         self.fc2 = nn.Linear(256, 256)
@@ -89,6 +119,9 @@ class QNetwork(nn.Module):
         x = self.fc3(x)
         return x
 
+    def save(self):
+        torch.save(self, f"{run_name}/models/critic.pt")
+
 
 class ExplorationCritic(nn.Module):
 
@@ -97,6 +130,9 @@ class ExplorationCritic(nn.Module):
 
     def forward(self, latent_state, latent_next_state, action):
         pass
+
+    def save(self):
+        torch.save(self, f"{run_name}/models/exploration_critic.pt")
 
 
 class StateAggregationEncoder(nn.Module):
@@ -115,8 +151,7 @@ class StateAggregationEncoder(nn.Module):
         self.batch_norm_3 = nn.BatchNorm1d(self.latent_size)
 
     def forward(self, state, value):
-        x = torch.flatten(state)
-        x = self.fc1(torch.concat([state, torch.scalar_tensor(value)], dim=-1))
+        x = self.fc1(torch.tensor(torch.hstack((state, value)), device=device))
         x = self.batch_norm_1(x)
         x = F.relu(x)
 
@@ -127,7 +162,10 @@ class StateAggregationEncoder(nn.Module):
         x = self.fc3(x)
         x = self.batch_norm_3(x)
 
-        return F.softmax(x)
+        return F.softmax(x).to(device)
+
+    def save(self):
+        torch.save(self, f"{run_name}/models/state_aggregation_encoder.pt")
 
 
 class StateAggregationDecoder(nn.Module):
@@ -154,6 +192,9 @@ class StateAggregationDecoder(nn.Module):
         x = self.fc3(x)
         return x
 
+    def save(self):
+        torch.save(self, f"{run_name}/models/state_aggregation_decoder.pt")
+
 
 class StateAggregationAutoEncoder(nn.Module):
     def __init__(self, env: gym.Env, latent_size=64):
@@ -163,22 +204,53 @@ class StateAggregationAutoEncoder(nn.Module):
         self.decoder = StateAggregationDecoder(env, latent_size=latent_size)
 
     def forward(self, state, value):
-        return self.decoder(self.encoder(state, value))
+        return self.decoder(torch.tensor(self.encoder(state, value), device=device))
 
-    def save_encoder(self):
-        torch.save(self.encoder, "models/state_aggregation_encoder.pt")
-
-    def save_decoder(self):
-        torch.save(self.decoder, "models/state_aggregation_decoder.pt")
-
-    def save_autoencoder(self):
-        torch.save(self, "models/state_aggregation_autoencoder.pt")
+    def save(self):
+        self.encoder.save()
+        self.decoder.save()
+        torch.save(self, f"{run_name}/models/state_aggregation_autoencoder.pt")
 
 
 class Actor(nn.Module):
     def __init__(self, env):
         super().__init__()
         self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod(), 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc_mu = nn.Linear(256, np.array(env.single_action_space.shape).prod())
+        self.fc_std = nn.Linear(256, np.array(env.single_action_space.shape).prod())
+
+        self.register_buffer(
+            "action_scale",
+            torch.tensor(
+                (env.action_space.high - env.action_space.low) / 2.0,
+                dtype=torch.float32,
+            ),
+        )
+
+        self.register_buffer(
+            "action_bias",
+            torch.tensor(
+                (env.action_space.high + env.action_space.low) / 2.0,
+                dtype=torch.float32,
+            ),
+        )
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        mu = self.fc_mu(x)
+        std = F.softplus(self.fc_std(x)) + 1e-6
+        return mu, std
+
+    def save(self):
+        torch.save(self, f"{run_name}/models/actor.pt")
+
+
+class CompressedActor(nn.Module):
+    def __init__(self, env, latent_size=64):
+        super().__init__()
+        self.fc1 = nn.Linear(latent_size, 256)
         self.fc2 = nn.Linear(256, 256)
         self.fc_mu = nn.Linear(256, np.prod(env.single_action_space.shape))
         # action rescaling
@@ -203,208 +275,158 @@ class Actor(nn.Module):
         x = torch.tanh(self.fc_mu(x))
         return x * self.action_scale + self.action_bias
 
+    def save(self):
+        torch.save(self, f"{run_name}/compressed_actor.pt")
 
-def train_state_aggregation_encoder(env_id, env_seed):
 
-    def approximate_value_function():
-        pass
+def train_actor_critic(actor=None, critic=None, total_timesteps=args.total_timesteps):
+    assert isinstance(
+        envs.single_action_space, gym.spaces.Box
+    ), "only continuous action space is supported"
 
-    env = gym.make(env_id)
-    env.observation_space.seed(env_seed)
-    env = env.unwrapped
+    if not actor:
+        actor = Actor(envs).to(device)
+        target_actor = Actor(envs).to(device)
+        target_actor.load_state_dict(actor.state_dict())
 
-    autoencoder = StateAggregationAutoEncoder()
-    critic = QNetwork(env)
+    if not critic:
+        qf1 = Critic(envs).to(device)
+        qf1_target = Critic(envs).to(device)
+        qf1_target.load_state_dict(qf1.state_dict())
+
+    q_optimizer = optim.Adam(list(qf1.parameters()), lr=args.learning_rate)
+    actor_optimizer = optim.Adam(list(actor.parameters()), lr=args.learning_rate)
+
+    rb = ReplayBuffer(
+        args.buffer_size,
+        envs.single_observation_space,
+        envs.single_action_space,
+        device,
+        handle_timeout_termination=False,
+    )
+
+    obs, _ = envs.reset(seed=args.seed)
+    for global_step in range(total_timesteps):
+        if global_step < args.learning_starts:
+            actions = np.array(
+                [envs.single_action_space.sample() for _ in range(envs.num_envs)]
+            )
+        else:
+            with torch.no_grad():
+                actions = actor(torch.Tensor(obs).to(device))
+                actions += torch.normal(0, actor.action_scale * args.exploration_noise)
+                actions = (
+                    actions.cpu()
+                    .numpy()
+                    .clip(envs.single_action_space.low, envs.single_action_space.high)
+                )
+
+        next_obs, rewards, terminations, truncations, infos = envs.step(actions)
+        rb.add(obs, next_obs, actions, rewards, terminations, infos)
+
+        if global_step > args.learning_starts:
+            data = rb.sample(args.batch_size)
+            with torch.no_grad():
+                next_state_actions = target_actor(data.next_observations)
+                qf1_next_target = qf1_target(data.next_observations, next_state_actions)
+                next_q_value = data.rewards.flatten() + (
+                    1 - data.dones.flatten()
+                ) * args.gamma * (qf1_next_target).view(-1)
+
+            qf1_a_values = qf1(data.observations, data.actions).view(-1)
+            qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
+
+            # optimize the model
+            q_optimizer.zero_grad()
+            qf1_loss.backward()
+            q_optimizer.step()
+
+            if global_step % args.policy_frequency == 0:
+                actor_loss = -qf1(data.observations, actor(data.observations)).mean()
+                actor_optimizer.zero_grad()
+                actor_loss.backward()
+                actor_optimizer.step()
+
+                # update the target network
+                for param, target_param in zip(
+                    actor.parameters(), target_actor.parameters()
+                ):
+                    target_param.data.copy_(
+                        args.tau * param.data + (1 - args.tau) * target_param.data
+                    )
+                for param, target_param in zip(
+                    qf1.parameters(), qf1_target.parameters()
+                ):
+                    target_param.data.copy_(
+                        args.tau * param.data + (1 - args.tau) * target_param.data
+                    )
+
+            if global_step % args.print_log_frequency == 0:
+                print(f"Critic Loss: {qf1_loss}, Actor Loss: {actor_loss}")
+
+    envs.close()
+    target_actor.save()
+    qf1_target.save()
+    return target_actor, qf1_target
+
+
+def train_state_aggregation_module(actor, critic):
+    autoencoder = StateAggregationAutoEncoder(envs)
+    BATCH_SIZE = 64
+
+    def approximate_value_function(state):
+        if isinstance(actor, CompressedActor):
+            state = autoencoder.encoder(state)
+
+        action_mean, action_std = actor(state)
+        dist = torch.distributions.Normal(action_mean, action_std)
+        # 1000 x 64 x 4
+        sampled_actions = dist.sample((1000,))
+        state = state.unsqueeze(0).expand(1000, -1, -1)
+        state = state.reshape(-1, critic.obs_space_size)
+
+        sampled_actions = sampled_actions.reshape(-1, 4)
+
+        q_values = critic(state, sampled_actions)
+        q_values = q_values.view(1000, 64, -1).mean(dim=0)
+
+        return torch.tensor(q_values, device=device)
 
     NUM_SAMPLES = 10000
-    BATCH_SIZE = 64
-    LEANRING_RATE = 0.001
+    LEANRING_RATE = 1e-2
     EPOCHS = 100
 
     optimizer = torch.optim.Adam(lr=LEANRING_RATE, params=autoencoder.parameters())
-    dataset = [env.observation_space.sample() for i in range(NUM_SAMPLES)]
+    dataset = [envs.single_observation_space.sample() for i in range(NUM_SAMPLES)]
     loss_fn = nn.MSELoss()
 
     for i in range(EPOCHS):
-
-        sample = torch.tensor(random.sample(dataset, BATCH_SIZE), dtype=torch.float32)
-        autoencoder_out = autoencoder(sample)
+        sample = torch.tensor(
+            random.sample(dataset, BATCH_SIZE), dtype=torch.float32, device=device
+        )
+        value = approximate_value_function(sample)
+        autoencoder_out = autoencoder(sample, value)
         loss = loss_fn(autoencoder_out, sample)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+        if i % args.print_log_frequency == 0:
+            print(f"State Aggregation Autoencoder Loss: {loss}")
+
+    return autoencoder.encoder, autoencoder.decoder, autoencoder
+
+
+def train_exploration_critic(actor, state_aggregation_encoder):
+    pass
+
 
 if __name__ == "__main__":
-    import stable_baselines3 as sb3
+    actor, critic = train_actor_critic()
 
-    if sb3.__version__ < "2.0":
-        raise ValueError(
-            """Ongoing migration: run the following command to install the new dependencies:
-poetry run pip install "stable_baselines3==2.0.0a1"
-"""
-        )
-    args = tyro.cli(Args)
-    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
-
-    writer = SummaryWriter(f"runs/{run_name}")
-    writer.add_text(
-        "hyperparameters",
-        "|param|value|\n|-|-|\n%s"
-        % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
-    )
-
-    # TRY NOT TO MODIFY: seeding
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.backends.cudnn.deterministic = args.torch_deterministic
-
-    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
-
-    # env setup
-    envs = gym.vector.SyncVectorEnv(
-        [make_env(args.env_id, args.seed, 0, args.capture_video, run_name)]
-    )
-    train_state_aggregation_encoder(args.env_id, args.seed)
-    # assert isinstance(
-    #     envs.single_action_space, gym.spaces.Box
-    # ), "only continuous action space is supported"
-
-    # actor = Actor(envs).to(device)
-    # qf1 = QNetwork(envs).to(device)
-    # qf1_target = QNetwork(envs).to(device)
-    # target_actor = Actor(envs).to(device)
-    # target_actor.load_state_dict(actor.state_dict())
-    # qf1_target.load_state_dict(qf1.state_dict())
-    # q_optimizer = optim.Adam(list(qf1.parameters()), lr=args.learning_rate)
-    # actor_optimizer = optim.Adam(list(actor.parameters()), lr=args.learning_rate)
-
-    # envs.single_observation_space.dtype = np.float32
-    # rb = ReplayBuffer(
-    #     args.buffer_size,
-    #     envs.single_observation_space,
-    #     envs.single_action_space,
-    #     device,
-    #     handle_timeout_termination=False,
-    # )
-    # start_time = time.time()
-
-    # # TRY NOT TO MODIFY: start the game
-    # obs, _ = envs.reset(seed=args.seed)
-    # for global_step in range(args.total_timesteps):
-    #     # ALGO LOGIC: put action logic here
-    #     if global_step < args.learning_starts:
-    #         actions = np.array(
-    #             [envs.single_action_space.sample() for _ in range(envs.num_envs)]
-    #         )
-    #     else:
-    #         with torch.no_grad():
-    #             actions = actor(torch.Tensor(obs).to(device))
-    #             actions += torch.normal(0, actor.action_scale * args.exploration_noise)
-    #             actions = (
-    #                 actions.cpu()
-    #                 .numpy()
-    #                 .clip(envs.single_action_space.low, envs.single_action_space.high)
-    #             )
-
-    #     # TRY NOT TO MODIFY: execute the game and log data.
-    #     next_obs, rewards, terminations, truncations, infos = envs.step(actions)
-
-    #     # TRY NOT TO MODIFY: record rewards for plotting purposes
-    #     if "final_info" in infos:
-    #         for info in infos["final_info"]:
-    #             print(
-    #                 f"global_step={global_step}, episodic_return={info['episode']['r']}"
-    #             )
-    #             writer.add_scalar(
-    #                 "charts/episodic_return", info["episode"]["r"], global_step
-    #             )
-    #             writer.add_scalar(
-    #                 "charts/episodic_length", info["episode"]["l"], global_step
-    #             )
-    #             break
-
-    #     # TRY NOT TO MODIFY: save data to reply buffer; handle `final_observation`
-    #     real_next_obs = next_obs.copy()
-    #     # for idx, trunc in enumerate(truncations):
-    #     #     if trunc:
-    #     #         real_next_obs[idx] = infos["final_observation"][idx]
-    #     rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
-
-    #     # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
-    #     obs = next_obs
-
-    #     # ALGO LOGIC: training.
-    #     if global_step > args.learning_starts:
-    #         data = rb.sample(args.batch_size)
-    #         with torch.no_grad():
-    #             next_state_actions = target_actor(data.next_observations)
-    #             qf1_next_target = qf1_target(data.next_observations, next_state_actions)
-    #             next_q_value = data.rewards.flatten() + (
-    #                 1 - data.dones.flatten()
-    #             ) * args.gamma * (qf1_next_target).view(-1)
-
-    #         qf1_a_values = qf1(data.observations, data.actions).view(-1)
-    #         qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
-
-    #         # optimize the model
-    #         q_optimizer.zero_grad()
-    #         qf1_loss.backward()
-    #         q_optimizer.step()
-
-    #         if global_step % args.policy_frequency == 0:
-    #             actor_loss = -qf1(data.observations, actor(data.observations)).mean()
-    #             actor_optimizer.zero_grad()
-    #             actor_loss.backward()
-    #             actor_optimizer.step()
-
-    #             # update the target network
-    #             for param, target_param in zip(
-    #                 actor.parameters(), target_actor.parameters()
-    #             ):
-    #                 target_param.data.copy_(
-    #                     args.tau * param.data + (1 - args.tau) * target_param.data
-    #                 )
-    #             for param, target_param in zip(
-    #                 qf1.parameters(), qf1_target.parameters()
-    #             ):
-    #                 target_param.data.copy_(
-    #                     args.tau * param.data + (1 - args.tau) * target_param.data
-    #                 )
-
-    #         if global_step % 100 == 0:
-    #             writer.add_scalar(
-    #                 "losses/qf1_values", qf1_a_values.mean().item(), global_step
-    #             )
-    #             writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
-    #             writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
-    #             print("SPS:", int(global_step / (time.time() - start_time)))
-    #             writer.add_scalar(
-    #                 "charts/SPS",
-    #                 int(global_step / (time.time() - start_time)),
-    #                 global_step,
-    #             )
-
-    # if args.save_model:
-    #     model_path = f"runs/{run_name}/{args.exp_name}.cleanrl_model"
-    #     torch.save((actor.state_dict(), qf1.state_dict()), model_path)
-    #     print(f"model saved to {model_path}")
-    #     from ddpg_eval import evaluate
-
-    #     episodic_returns = evaluate(
-    #         model_path,
-    #         make_env,
-    #         args.env_id,
-    #         eval_episodes=10,
-    #         run_name=f"{run_name}-eval",
-    #         Model=(Actor, QNetwork),
-    #         device=device,
-    #         exploration_noise=args.exploration_noise,
-    #     )
-    #     for idx, episodic_return in enumerate(episodic_returns):
-    #         writer.add_scalar("eval/episodic_return", episodic_return, idx)
-
-    # envs.close()
-    # writer.close()
+    pipeline_loops = 10
+    for i in range(pipeline_loops):
+        state_aggregation_encoder, _, _ = train_state_aggregation_module(actor, critic)
+        # exploration_critic = train_exploration_critic(actor, state_aggregation_encoder)
+        # actor, critic = train_actor_critic(actor, critic, exploration_critic)
